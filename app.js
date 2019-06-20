280ab76004d9495bf0df201004ae12d962c2de45
Harin <teri941227@gmail.com>
2019-06-20 14:55:41 +0900

    commit first

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 var http = require('http');
-var fs=require("fs");
-// var hostname = '127.0.0.1';
-// var port = 3000;
-var server = http.createServer(function(req, res){
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html;charset=utf-8');
-  let url="";
-  if(req.url == "/"){
-    url="/index.html";
-  }else{
-    url=req.url;
-  }
-  res.end(fs.readFileSync(__dirname + url));
-});
+// var fs=require("fs");
+var ex=require("express");
+var static=require("serve-static");
+var path=require("path");
+var app=ex();
+app.use(static(__dirname+"/static"));
+var server = http.createServer(app);
 
 server.listen(80, () => {
   console.log(`Server running at`);
